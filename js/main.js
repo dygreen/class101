bf3f0007ee5ac27d708d10e4eece6e58c56a313b
dygreen <95523625+dygreen@users.noreply.github.com>
2022-05-23 21:18:46 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -66,14 +66,56 @@ $(document).ready(function(){
   // accordion menu
   $("dd:not(:first)").css("display","none");
   $("dt:first").addClass("selected");
-
+  
   $("dl dt").click(function(){
     $("dl dt").find("+dd").stop().slideUp(200);
     $(this).find("+dd").stop().slideToggle(200);
     $("dt").removeClass("selected");
     $(this).addClass("selected");
   });
-
-
   
 });
+
+// =========== 1~5 category ===========
+// 더보기 버튼 클릭시, 추가 컨텐츠 등장+더보기 버튼 숨기기 -> onload로 실행
+function view_more(){
+  $("section#class_section button").click(function(){
+    $("section#class_section ul li[data-index='4'], section#class_section ul li[data-index='5'], section#class_section ul li[data-index='6'], section#class_section ul li[data-index='7']").slideDown("slow");
+    $("section#class_section .more_btn").fadeOut();
+  });
+}
+
+// 데이터바인딩(json파일에 있는 데이터를 ajax get 요청하기) -> onload로 실행
+function getClass() {
+  $.get("resources/class1.json").done((data) => {
+    data.classes.forEach((card, i) => {
+      appendProducts(card, i);
+    });
+  });
+}
+function getClass2() {
+  $.get("resources/class2.json").done((data) => {
+    data.classes.forEach((card, i) => {
+      appendProducts(card, i);
+    });
+  });
+}
+
+// 상품 데이터바인딩 코드, drag 기능 가능하도록 설정(jquery UI)
+function appendProducts(classes, index) {
+  let newItem = $(`
+  <li data-index="${index}">
+    <img src="resources/${classes.photo}" alt="class photo" class="class_img">
+    <div class="text">
+      <p class="class_name">${classes.name}</p>
+      <p class="class_sub clamp">${classes.sub}</p>
+      <hr class="class_line">
+      <p class="class_price">
+        <span class="sale">${classes.sale}%</span>${classes.price}<span class="month">${classes.month}</span>
+      </p>
+    </div>
+  </li>
+  `);
+
+  $(".class_card").append(newItem);
+}
