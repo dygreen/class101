57e703ade04abe1e032e118f0d16c860531c1c83
dygreen <95523625+dygreen@users.noreply.github.com>
2022-05-26 21:13:17 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 // SEARCH ) 실시간 급상승 검색어 롤링
-function rolling(){
-  $(".rising_up ul").stop(true,true).animate({"margin-top":"-=28px"},500,function(){
+function rolling() {
+  $(".rising_up ul").stop(true, true).animate({
+    "margin-top": "-=28px"
+  }, 500, function () {
     $(".rising_up ul li:first-child").appendTo(".rising_up ul");
-    $(this).css({"margin-top":"0"});
+    $(this).css({
+      "margin-top": "0"
+    });
   });
 }
 
+// INDEX) 스플래쉬
+function splashIn(){
+  $(".splash").fadeOut(3000);
+}
 
+$(document).ready(function () {
 
-$(document).ready(function(){
-
-  setInterval(rolling,3000);
+  setInterval(rolling, 3000);
 
   // top btn
-  $(window).scroll(function(){
-    if($(window).scrollTop() > 100){
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
       $("#top_btn").fadeIn();
     } else {
       $("#top_btn").fadeOut();
@@ -22,16 +29,18 @@ $(document).ready(function(){
   });
 
   // top btn 클릭시 부드럽게 올라가는
-  $("#top_btn").click(function(){
-    $("html, body").animate({scrollTop : 0}, 300);
+  $("#top_btn").click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 300);
     return false;
   });
 
   // =========== SEARCH ===========
   // 실시간 급상승 icon을 클릭하면 전체 리스트가 보이는
   let rising = true;
-  $(".rising .down_icon").click(function(){
-    if(rising){
+  $(".rising .down_icon").click(function () {
+    if (rising) {
       $(this).html('<span class="iconify" data-icon="akar-icons:circle-chevron-up-fill"></span>');
       $(".rising_up").fadeOut();
       $(".rising_all").slideDown("slow");
@@ -49,13 +58,13 @@ $(document).ready(function(){
   let button = $("#event_tab .tab_button>li");
   let content = $("#event_tab .tab_content>li");
 
-  for(let i = 0; i < button.length; i++){
-    button.eq(i).click(function(){
+  for (let i = 0; i < button.length; i++) {
+    button.eq(i).click(function () {
       tab(i);
     });
   }
 
-  function tab(i){
+  function tab(i) {
     button.removeClass("selected");
     button.eq(i).addClass("selected");
     content.removeClass("show");
@@ -64,22 +73,43 @@ $(document).ready(function(){
 
   // =========== MY PAGE ===========
   // accordion menu
-  $("dd:not(:first)").css("display","none");
+  $("dd:not(:first)").css("display", "none");
   $("dt:first").addClass("selected");
-  
-  $("dl dt").click(function(){
+
+  $("dl dt").click(function () {
     $("dl dt").find("+dd").stop().slideUp(200);
     $(this).find("+dd").stop().slideToggle(200);
     $("dt").removeClass("selected");
     $(this).addClass("selected");
   });
-  
+
+  // =========== INDEX ===========
+  // index 클래스 안내 함수 실행
+  newEdu();
+  dealEdu();
+  mdEdu();
+
+  // quick menu: toggle 버튼 클릭시 메뉴 등장, toggle 버튼 텍스트 바뀜
+  let toggle = true;
+  $(".quick_toggle").click(function(){
+    if(toggle){
+      $(".quick_list li:first-child").stop(true,true).animate({"bottom":"62px"},500,"easeOutBack");
+			$(".quick_list li:nth-child(2)").stop(true,true).animate({"bottom":"125px"},500,"easeOutBack");
+			$(".quick_list li:last-child").stop(true,true).animate({"bottom":"188px"},500,"easeOutBack");
+      $(this).text("×");
+      toggle = false;
+    } else {
+      $(".quick_list li").stop(true,true).animate({"bottom":"0px"},200);
+      $(this).text("?");
+      toggle = true;
+    }
+  });
 });
 
 // =========== 1~5 category ===========
 // 더보기 버튼 클릭시, 추가 컨텐츠 등장+더보기 버튼 숨기기 -> onload로 실행
-function view_more(){
-  $("section#class_section button").click(function(){
+function view_more() {
+  $("section#class_section button").click(function () {
     $("section#class_section ul li[data-index='4'], section#class_section ul li[data-index='5'], section#class_section ul li[data-index='6'], section#class_section ul li[data-index='7']").slideDown("slow");
     $("section#class_section .more_btn").fadeOut();
   });
@@ -122,7 +152,7 @@ function getClass5() {
   });
 }
 
-// 클래스 데이터바인딩 코드
+// 클래스 데이터바인딩 코드(category)
 function appendProducts(classes, index) {
   let newItem = $(`
   <li data-index="${index}">
@@ -139,4 +169,80 @@ function appendProducts(classes, index) {
   `);
 
   $(".class_card").append(newItem);
+}
+
+// ============= index =============
+function newEdu() {
+  $.get("resources/class1.json").done((data) => {
+    data.classes.forEach((card, i) => {
+      appendEdu(card, i);
+    });
+  });
+}
+function dealEdu() {
+  $.get("resources/class2.json").done((data) => {
+    data.classes.forEach((card, i) => {
+      appendEdu2(card, i);
+    });
+  });
+}
+function mdEdu() {
+  $.get("resources/class3.json").done((data) => {
+    data.classes.forEach((card, i) => {
+      appendEdu3(card, i);
+    });
+  });
+}
+
+// 클래스 데이터바인딩 코드(index)
+function appendEdu(classes, index) {
+  let newItem2 = $(`
+  <li class="swiper-slide" data-index="${index}">
+    <img src="resources/${classes.photo}" alt="class photo" class="class_img">
+    <div class="text">
+      <p class="class_name">${classes.name}</p>
+      <p class="class_sub clamp">${classes.sub}</p>
+      <hr class="class_line">
+      <p class="class_price">
+        <span class="sale">${classes.sale}%</span>${classes.price}
+      </p>
+    </div>
+  </li>
+  `);
+
+  $("#index_edu .new_edu .swiper-wrapper").append(newItem2);
+}
+function appendEdu2(classes, index) {
+  let newItem2 = $(`
+  <li class="swiper-slide" data-index="${index}">
+    <img src="resources/${classes.photo}" alt="class photo" class="class_img">
+    <div class="text">
+      <p class="class_name">${classes.name}</p>
+      <p class="class_sub clamp">${classes.sub}</p>
+      <hr class="class_line">
+      <p class="class_price">
+        <span class="sale">${classes.sale}%</span>${classes.price}
+      </p>
+    </div>
+  </li>
+  `);
+
+  $("#index_edu .deal_edu .swiper-wrapper").append(newItem2);
+}
+function appendEdu3(classes, index) {
+  let newItem2 = $(`
+  <li class="swiper-slide" data-index="${index}">
+    <img src="resources/${classes.photo}" alt="class photo" class="class_img">
+    <div class="text">
+      <p class="class_name">${classes.name}</p>
+      <p class="class_sub clamp">${classes.sub}</p>
+      <hr class="class_line">
+      <p class="class_price">
+        <span class="sale">${classes.sale}%</span>${classes.price}
+      </p>
+    </div>
+  </li>
+  `);
+
+  $("#index_edu .md_edu .swiper-wrapper").append(newItem2);
 }
